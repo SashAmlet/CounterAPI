8bb722398c8399082e5d2af001aa11bb21d88484
SashAmlet <ostrenkosasha@gmail.com>
2023-04-30 17:17:38 +0300

    <fuck>

--- wwwroot/js/UpdateLanguage_Theme.js
+++ wwwroot/js/UpdateLanguage_Theme.js
@@ -1,30 +1,10 @@
-﻿function _displayUserAddLanguageList(data) {
-    const tBody = document.getElementById('user-add-language-list');
+﻿function _displayUserAddLanguageThemeList(data, tbodyId, selectId) {
+    const tBody = document.getElementById(tbodyId);
     tBody.innerHTML = '';
 
     if (Array.isArray(data)) {
         let selectNodeUsers = document.createElement('select');
-        selectNodeUsers.setAttribute('id', 'user-select-language');
-        data.forEach(language => {
-
-            let option = document.createElement('option');
-            option.value = language.name;
-            option.text = language.name;
-            selectNodeUsers.appendChild(option);
-
-        });
-
-        tBody.appendChild(selectNodeUsers);
-    }
-}
-
-function _displayUserAddThemeList(data) {
-    const tBody = document.getElementById('user-add-theme-list');
-    tBody.innerHTML = '';
-
-    if (Array.isArray(data)) {
-        let selectNodeUsers = document.createElement('select');
-        selectNodeUsers.setAttribute('id', 'user-select-theme');
+        selectNodeUsers.setAttribute('id', selectId);
         data.forEach(language => {
 
             let option = document.createElement('option');

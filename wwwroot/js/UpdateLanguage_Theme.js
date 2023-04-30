eb100f71c566a9533666f19ef26e15eb3add9991
SashAmlet <ostrenkosasha@gmail.com>
2023-04-30 17:17:51 +0300

    Revert "<fuck>"
    This reverts commit 8bb722398c8399082e5d2af001aa11bb21d88484.

--- wwwroot/js/UpdateLanguage_Theme.js
+++ wwwroot/js/UpdateLanguage_Theme.js
@@ -1,10 +1,30 @@
-﻿function _displayUserAddLanguageThemeList(data, tbodyId, selectId) {
-    const tBody = document.getElementById(tbodyId);
+﻿function _displayUserAddLanguageList(data) {
+    const tBody = document.getElementById('user-add-language-list');
     tBody.innerHTML = '';
 
     if (Array.isArray(data)) {
         let selectNodeUsers = document.createElement('select');
-        selectNodeUsers.setAttribute('id', selectId);
+        selectNodeUsers.setAttribute('id', 'user-select-language');
+        data.forEach(language => {
+
+            let option = document.createElement('option');
+            option.value = language.name;
+            option.text = language.name;
+            selectNodeUsers.appendChild(option);
+
+        });
+
+        tBody.appendChild(selectNodeUsers);
+    }
+}
+
+function _displayUserAddThemeList(data) {
+    const tBody = document.getElementById('user-add-theme-list');
+    tBody.innerHTML = '';
+
+    if (Array.isArray(data)) {
+        let selectNodeUsers = document.createElement('select');
+        selectNodeUsers.setAttribute('id', 'user-select-theme');
         data.forEach(language => {
 
             let option = document.createElement('option');
